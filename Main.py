c14ce9f6f25a9c287449a0e7a0a67b6c7c71dd82
Omar Najera <omar.navzz@gmail.com>
2020-11-05 16:20:54 -0600

    Se modifico el main para mayor rapidez

--- Main.py
+++ Main.py
@@ -19,76 +19,65 @@ while True:
 """)
     op = input("Insertar opción: ")
     
-    if op == "1":
-        print(f'\n El directorio actual es: {os.getcwd()}{cut}')
+    if op == "4":
+        print(f"\n¡Hasta pronto {usuario}!")
+        break
+
+    elif op == "1":
+        print(f'\nEl directorio actual es: {os.getcwd()}{cut}')
 
     elif op == "2":
         user = input(dirnew)
+
         if user == os.getcwd():
             print("Esta tratando de ir al directorio donde se encuentra actualmente")
+
         else:
             try:
                 print(f"El directorio '{os.getcwd()}' cambiará a '{user}'\n")
                 diruser = directorio(newdir=user)
                 diruser.changedir()
-                print("\nEl directorio cambio correctamente. \n")
-                print(f'\n El directorio actual es: {os.getcwd()}{cut}')
+                print(f"\nEl directorio cambio correctamente. \nEl directorio actual es: {os.getcwd()}{cut}")
+                
             except:
                 print("Directorio Inválido. Intente de nuevo.")
                 
     elif op == "3":
-        print(f'\n El directorio actual es: {os.getcwd()}{cut}')
+        print(f'\nEl directorio actual es: {os.getcwd()}{cut}')
         n = directorio(nowdir=os.getcwd()).showfile()
         con = 0
         print("Archivos de tu carpeta: ")
 
         if len(n) > 0:
+
             for a in n:
                 con += 1
                 print(f"{con}: {a}")
 
             try:
-                archi = int(input("\n Seleccione un archivo de la anterior lista: "))
+                archi = int(input("\nSeleccione un archivo de la anterior lista: "))
+
                 if (archi - 1) in range(len(n)):
                     archivo = n[(archi-1)]
 
                     while True:
                         op2 = input("¿Qué deseas hacer?\n1. Copiar Archivo\n2. Mover Archivo\n")
+                        move = input(dirnew)
+    
+                        try:
+                            if op2 == "1":
+                                directorio(newdir=move,arcsel=archivo).copy()
+                                break
 
-                        if op2 == "1":
-
-                            while True:
-                                op3 = input("Elegir\n1. Copiar en la misma carpeta.\n2. Copiar en otro directorio.\n")
-
-                                if op3 == "1":
-                                    directorio(newdir=os.getcwd(),arcsel=archivo).copy()
-                                    break
-
-                                elif op3 == "2":
-                                    move = input(dirnew)
-                                    try:
-                                        directorio(newdir=move,arcsel=archivo).copy()
-                                        break
-                                    except:
-                                        print("No existe el directorio a cambiar.")
-
-                                else:
-                                    print(f"Opción {op3} no es válida.")
-                                
-                            break
-
-                        elif op2 == "2":
-                            move = input(dirnew)
-                            try:
+                            elif op2 == "2":
                                 directorio(newdir=move,arcsel=archivo).move()
                                 break
 
-                            except:
-                                print("No existe el directorio a cambiar.")
-                            
+                            else:
+                                print(f"Opción {op2} no válida.")
 
-                        else:
-                            print(f"Opción {op2} no es válida.")
+                        except:
+                            print("No existe el directorio a cambiar.")     
 
                 else:
                     print(f"No hay archivo número {archi}.")
@@ -96,15 +85,12 @@ while True:
             except:
                 print(f"Ocurrió un problema {sys.exc_info()[0]}")
                 Excepcion = sys.exc_info()
+
                 for elemento in Excepcion:
                     print(elemento)
             
         else:
             print("No hay archivos en la carpeta actual.")
-
-    elif op == "4":
-        print(f"\n¡Hasta pronto {usuario}!")
-        break
 
     else:
         print(f"La opción '{op}' no es válida.\nEliga una de las opciones correctas.{cut}")
